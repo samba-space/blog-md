11b515114acfe2c9a0b28a6793085e3707daf8e2
Samba <junkipark430@gmail.com>
2021-07-13 11:17:55 +0900

    project 추가

---
+++ modern-java/modern-java/src/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Main.main");
+    }
+}
---
+++ modern-java/modern-java/src/com/example2/Main2.java
@@ -0,0 +1,7 @@
+package com.example2;
+
+public class Main2 {
+    public static void main(String[] args) {
+        System.out.println("main2");
+    }
+}
