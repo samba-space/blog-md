1b4192cc06f40f9cd2e93d181c28cee59ad0430f
Samba <junkipark430@gmail.com>
2021-07-15 23:08:35 +0900

    예제

--- modern-java/modern-java/src/com/example/Main.java
+++ modern-java/modern-java/src/com/example/Main.java
@@ -1,13 +1,28 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.ToIntBiFunction;
 
 public class Main {
-    private class Apple{
+    private static int number;
+    private static class Apple{
         private Color color;
         private int weight;
+
+        public Apple(int weight) {
+            this.weight = weight;
+        }
+
+        public Apple(Color color, int weight) {
+            this.color = color;
+            this.weight = weight;
+        }
 
         public int getWeight() {
             return weight;
@@ -32,8 +47,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Main.main");
+        List<Apple> inventory = new ArrayList<>();
         List<Apple> greenApples = filterApples(inventory, Color.GREEN, 0, true);
         List<Apple> heavyApples = filterApples(inventory, Color.RED, 150, false);
+
+        filterApples(inventory, apple -> Color.GREEN.equals(apple.getColor()));
+        inventory.sort(Comparator.comparing(Apple::getWeight));
+        ToIntBiFunction<String, String> compareToIgnoreCase = String::compareToIgnoreCase;
+
+        Function<Integer, Apple> colorIntegerAppleBiFunction = Apple::new;
+        BiFunction<Color, Integer, Apple> colorIntegerAppleBiFunction1 = Apple::new;
     }
 
     public static List<Apple> filterGreenApples(List<Apple> inventory){
@@ -62,6 +85,16 @@ public class Main {
         List<Apple> result = new ArrayList<>();
         for (Apple apple : inventory) {
             if((flag && apple.getColor().equals(color)) || (!flag && apple.getWeight() > weight)){
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> p){
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory) {
+            if(p.test(apple)){
                 result.add(apple);
             }
         }
---
+++ modern-java/modern-java/src/com/example3/Main3.java
@@ -0,0 +1,48 @@
+package com.example3;
+
+import com.sun.corba.se.impl.orbutil.ObjectUtility;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.*;
+
+public class Main3 {
+    @FunctionalInterface
+    public interface BufferedReaderProcessor{
+        String process(BufferedReader b) throws IOException;
+    }
+    public static void main(String[] args) throws IOException {
+        String oneLine = processFile((BufferedReader br) -> br.readLine());
+        String twoLines = processFile((BufferedReader br) -> br.readLine() + br.readLine());
+
+        IntPredicate evenNumbers = (int i) -> i % 2 == 0;
+
+        List<String> list = new ArrayList<>();
+        Predicate<String> p = s -> list.add(s);
+        Consumer<String> c = s -> list.add(s);
+        Function<String, Boolean> filter = (String s) -> s.isEmpty();
+        Consumer<String> cs = (String s) -> s.isEmpty();
+
+        int portNumber = 1;
+        Runnable runnable = () -> System.out.println("portNumber = " + portNumber);
+        portNumber = 2;
+
+        ToIntFunction<String> function = Integer::parseInt;
+        BiPredicate<List, Object> contains = List::contains;
+
+        list.sort((o1, o2) -> 0);
+
+
+    }
+    public static String processFile(BufferedReaderProcessor p) throws IOException{
+        try(BufferedReader br = new BufferedReader(new FileReader("data.txt"))){
+            return p.process(br);
+        }
+    }
+
+}
